387f86b7cd44bc4956aae5dcf207bced6a61457a
Guan Ce <ithinco@gmail.com>
2016-04-23 21:42:33 +0800

    to meet specifications

--- Website Optimization/views/js/main.js
+++ Website Optimization/views/js/main.js
@@ -530,7 +530,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < Math.floor(window.innerHeight*5/256); i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
